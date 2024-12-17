d4eed20bea50c12a903ce3d03458b2e9307834d9
Dmitriy <plotnikovdi95@gmail.com>
2024-12-17 21:12:45 +0300

    изменение стилей

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,23 +1,60 @@
 import './JournalForm.css'
 
 import Button from '../Button/Button.jsx'
+import { useState } from 'react'
 
 const JournalForm = ({ onSubmit }) => {
+  const [formValidState, setFormValidState] = useState({
+    title: true,
+    text: true,
+    date: true
+  })
+
   const addJournalItem = e => {
     e.preventDefault()
 
     const formData = new FormData(e.target)
     const formProps = Object.fromEntries(formData)
-    console.log('formProps: ', formProps)
+
+    let isFormValid = true
+
+    if (!formProps.title?.trim().length) {
+      setFormValidState(state => ({ ...state, title: false }))
+      isFormValid = false
+    } else {
+      setFormValidState(state => ({ ...state, title: true }))
+    }
+    if (!formProps.text?.trim().length) {
+      setFormValidState(state => ({ ...state, text: false }))
+      isFormValid = false
+    } else {
+      setFormValidState(state => ({ ...state, text: true }))
+    }
+    if (!formProps.date) {
+      setFormValidState(state => ({ ...state, title: false }))
+      isFormValid = false
+    } else {
+      setFormValidState(state => ({ ...state, text: true }))
+    }
+
+    if (!isFormValid) return
+
     onSubmit(formProps)
   }
 
   return (
     <form action="journal-form" onSubmit={addJournalItem}>
-      <input type="text" name="title" />
-      <input type="date" name="date" />
+      <input type="text" name="title" style={{ border: formValidState.title ? undefined : '1px solid red' }} />
+      <input type="date" name="date" style={{ border: formValidState.date ? undefined : '1px solid red' }} />
       <input type="text" name="tag" />
-      <textarea name="text" id="journal-text" cols="30" rows="10" placeholder="Текст журнала"></textarea>
+      <textarea
+        name="text"
+        id="journal-text"
+        cols="30"
+        rows="10"
+        placeholder="Текст журнала"
+        style={{ border: formValidState.text ? undefined : '1px solid red' }}
+      ></textarea>
       <Button text="Сохранить" />
     </form>
   )
